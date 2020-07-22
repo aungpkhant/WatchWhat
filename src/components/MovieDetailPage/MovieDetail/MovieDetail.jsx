0aba874f9a5aec96f2f843a61c4df5e7f1086379
AungPhoneKhant <akha0030@student.monash.edu>
2020-07-22 15:11:36 +1000

    Added genres

--- src/components/MovieDetailPage/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetailPage/MovieDetail/MovieDetail.jsx
@@ -2,24 +2,36 @@ import React, { useEffect } from "react";
 import styles from "./MovieDetail.module.css";
 import { useSelector } from "react-redux";
 
+const convertToHourAndMinutes = (timeInMin) => {
+    const hours = Math.floor(timeInMin / 60);
+    const minutes = timeInMin % 60;
+    return `${hours}h ${minutes}m`;
+};
+
 export default function MovieDetail(props) {
     const movie = useSelector((state) => state.movie);
+
+    const genres = movie
+        ? movie.genres.map((genre) => genre.name).join(", ")
+        : null;
 
     const moviePage = (
         <div className={styles.container}>
             <h1>{movie.title}</h1>
             <h5 className={styles.tagline}>{`"${movie.tagline}"`}</h5>
-            <div className={styles.genres}></div>
             <div className={styles.rating}>
                 <i className="fas fa-stopwatch"></i>
-                {movie.runtime}
+                {convertToHourAndMinutes(movie.runtime)}
                 <span className={styles.star}>
                     <i className="fas fa-star"></i>
                     {movie.vote_average}
                 </span>
             </div>
+            <div className={styles.genres}>
+                <h4>{genres}</h4>
+            </div>
             <div className={styles.overview}>
-                <h4>Overview</h4>
+                <h4 className={styles.textMuted}>Overview</h4>
                 {movie.overview}
             </div>
         </div>
