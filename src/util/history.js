27f9010fc03662d91eab25b591476d94bb5d80a6
AungPhoneKhant <akha0030@student.monash.edu>
2020-07-27 17:52:42 +1000

    Changed Base url

--- src/util/history.js
+++ src/util/history.js
@@ -1,15 +1,5 @@
 import { createBrowserHistory } from "history";
 
-let history;
-
-if (process.env.NODE_ENV == "development") {
-    history = createBrowserHistory({
-        basename: "/WatchWhat",
-    });
-} else if (process.env.NODE_ENV == "production") {
-    history = createBrowserHistory({
-        basename: "/WatchWhat",
-    });
-}
+const history = createBrowserHistory();
 
 export default history;
