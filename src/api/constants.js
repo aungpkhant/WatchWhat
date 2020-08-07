09c3cffd4cabdbca944a4b7af73cd3cebb19b46a
AungPhoneKhant <akha0030@student.monash.edu>
2020-08-07 20:08:20 +1000

    Faster search

--- src/api/constants.js
+++ src/api/constants.js
@@ -9,9 +9,8 @@ export const api_img_url_OG = "https://image.tmdb.org/t/p/original";
 export const youtube_base_url = "https://www.googleapis.com/youtube/v3/";
 export const API_URL = "https://api.themoviedb.org/3/";
 export const YOUTUBE_EMBED_LINK = "https://www.youtube.com/embed/";
-export const CORS_ANYWHERE_LINK = "https://cors-anywhere.herokuapp.com/";
 export const TMDB_QUERY_LINK = (query) =>
-    `${CORS_ANYWHERE_LINK}${API_URL}search/movie?query=${query}&api_key=${TMDB_API_KEY}`;
+    `${API_URL}search/movie?query=${query}&api_key=${TMDB_API_KEY}`;
 
 export const youtube = Axios.create({
     baseURL: youtube_base_url,
